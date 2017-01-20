6287e138bcafad905fc882125c548a76e30d243a
SylveinFARAUX <sylveinfaraux@hotmail.com>
2017-01-20 01:41:13 +0100

    timer prit en compte
    le raspi ne lit le même uid qu'après 3sec

--- Divers/py_client.py
+++ Divers/py_client.py
@@ -4,23 +4,25 @@ import time
 from urllib import urlencode
 
 url= "http://10.42.0.1/client.php"
-
+temps=0
+ancien_uid= "petitTasDeMerde"
 mifare=nxppy.Mifare()
 try:
-        while True:
-                try:
-                        uid = mifare.select()
-                        print "Read uid", uid
+	while True:
+		try:
+			uid= mifare.select()
+        		if (uid != ancien_uid) or ((time.time() - temps) >3):
+				ancien_uid= uid
+				temps= time.time()
+				#print "Read uid", uid
+				params = {'pseudo': uid}
+				http_params = urlencode(params)
 
-                        params = {'pseudo': uid}
-                        http_params = urlencode(params)
+				req= urllib2.Request(url, http_params)
+				connection = urllib2.urlopen(req)
 
-                        req= urllib2.Request(url, http_params)
-                        connection = urllib2.urlopen(req)
-
-                except nxppy.SelectError:
-                        pass
-                time.sleep(0.2)
+		except nxppy.SelectError:
+        		pass
+	 	time.sleep(0.2)   
 except KeyboardInterrupt:
         pass
-
